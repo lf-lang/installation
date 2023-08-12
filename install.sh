adffeb0c04a72ad1bc0daba1a7bec26a26936d6f
Marten Lohstroh <marten@berkeley.edu>
2023-08-11 22:54:41 -0700

    Several improvements

--- install.sh
+++ install.sh
@@ -1,56 +1,157 @@
 #!/bin/bash
+set -eo pipefail
 
-# Installer for Lingua Franca CLI tools.
+# Installer for Lingua Franca tools.
 # 
-# Usage: curl -s https://lf-lang.org/install-cli.sh | bash -s nightly
+# To install the stable CLI tools, run:
+# curl -s https://install.lf-lang.org | bash -s cli
+# To install the nightly CLI tools, run:
+# curl -s https://install.lf-lang.org | bash -s nightly cli
+#
+# To install the stable release of Epoch, run:
+# curl -s https://install.lf-lang.org | bash -s epoch
+# To install the nightly release of Epoch, run:
+# curl -s https://install.lf-lang.org | bash -s nightly epoch
 
-if [[ $1 == "nightly" ]]; then
-  kind="nightly"
-  rel="https://api.github.com/repos/lf-lang/lingua-franca/releases/tags/nightly"
-else
+tools=(cli epoch)
+selected=()
+
+install() (
+  case $1 in
+    cli)
+      cp -rf $dir/bin/* $prefix/bin/
+      cp -rf $dir/lib/* $prefix/lib/
+    ;;
+  esac
+  echo "    - Installed: $(ls --format=commas $dir/bin/)"
+)
+
+cleanup() (
+  case $1 in
+    cli)
+      rm -rf $dir
+    ;;
+  esac
+)
+
+download() (
+  case $1 in
+    cli)
+      echo "    - Unpacking into $tmp"
+      curl -sL $url | tar xfz - -C $tmp
+    ;;
+  esac
+)
+
+# Parse arguments
+for i in "$@"
+do
+# FIXME: add -p/--prefix
+case $i in
+    all)
+    selected=$all_tools
+    ;;
+    cli)
+    selected+=("cli")
+    ;;
+    epoch)
+    selected+=("epoch")
+    ;;
+    nightly)
+    if [[ -z $kind ]]; then
+      kind="nightly"
+    else
+      echo "You can only use one qualifier; choose 'stable' or 'nightly'."
+      exit 1
+    fi
+    ;;
+    stable)
+    if [[ -z $kind ]]; then
+      kind="stable"
+    else
+      echo "You can only use one qualifier; choose 'stable' or 'nightly'."
+      exit 1
+    fi
+    ;;
+    *)
+    echo "Unknown option: $i"
+    exit 1
+    ;;
+esac
+done
+
+# Use stable by default
+if [[ -z $kind ]]; then
   kind="stable"
-  rel="https://api.github.com/repos/lf-lang/lingua-franca/releases/latest"
 fi
 
-echo "Installing latest $kind Lingua Franca release..."
+# Use ~/.local default prefix
+if [[ -z $prefix ]]; then
+  prefix=~/.local
+fi
+
+# FIXME: add option to override
+tmp="/tmp"
+
+# Require a tool to be selected
+if [ ${#selected[@]} -eq 0 ]; then
+  echo "Please specify a tool to install. To install all tools, use 'all'."
+  exit 1
+fi
+
+# Create installation directories if necessary
+if [ ! -d $prefix/bin ]; then
+  echo "> Creating directory $prefix/bin"
+  mkdir -p $prefix/bin;
+fi
+
+if [ ! -d $prefix/lib ]; then
+  echo "> Creating directory $prefix/lib"
+  mkdir -p $prefix/lib;
+fi
+
+echo $prefix
+echo $kind
+echo ${selected[0]}
+
+for tool in "${selected[@]}"; do
+  case $tool in
+    cli)
+      description="CLI tools"
+      if [ "$kind" = "nightly" ]; then
+        rel="https://api.github.com/repos/lf-lang/lingua-franca/releases/tags/nightly"
+        kvp=$(curl -L -H "Accept: application/vnd.github+json" $rel 2>&1 | grep download_url | grep Linux-aarch64.tar.gz)
+      else
+        rel="https://api.github.com/repos/lf-lang/lingua-franca/releases/latest"
+        kvp=$(curl -L -H "Accept: application/vnd.github+json" $rel 2>&1 | grep download_url | grep lf-cli-0.4.0.tar.gz)
+      fi
+      arr=($kvp)
+      url="${arr[1]:1:-1}"
+      file=$(echo $url | grep -o '[^/]*\.tar.gz')
+      dir=$tmp/"${file%.tar.gz}"
+    ;;
+    *)
+      echo "> Unable to install $tool."
+      continue
+    ;;
+  esac
+
+  echo "> Installing the latest $kind $description release of $tool..."
+  echo ""
+
+  # Download
+  cleanup $tool
+  echo "  * Downloading from $url"
+  download $tool
+
+  # Install and remove tmp files
+  echo "  * Installing from $tmp into $prefix"
+  install $tool
+  echo "  * Removing temporary files"
+  cleanup $tool
+
+  echo ""
+done
+
+echo "> Done. Please ensure that $prefix/bin is on your PATH."
 echo ""
-
-# Determine download URL
-kvp=$(curl -L -H "Accept: application/vnd.github+json" $rel 2>&1 | grep download_url | grep Linux-aarch64.tar.gz)
-arr=($kvp)
-url="${arr[1]:1:-1}"
-
-# Download and unpack in /tmp
-echo "> Downloading from $url"
-echo "> Upacking into /tmp"
-rm -rf /tmp/lf-cli*
-curl -sL $url | tar xfz - -C /tmp #xz
-
-# Create local installation directories if necessary
-if [ ! -d ~/.local/bin ]; then
-  mkdir -p ~/.local/bin;
-fi
-
-if [ ! -d ~/.local/lib ]; then
-  mkdir -p ~/.local/lib;
-fi
-
-# Install and remove tmp files
-echo "> Installing from /tmp into ~/.local"
-cp -f /tmp/lf-cli*/bin/* ~/.local/bin/
-cp -f /tmp/lf-cli*/lib/* ~/.local/lib/
-echo "> Removing temporary files"
-rm -rf /tmp/lf-cli*
-
-# Add ~/.local/bin to PATH if necessary
-echo "> Configuring PATH"
-if echo $PATH | grep -q ~/.local/bin; then
-  echo "  ~/.local/bin already found on PATH"
-else
-  echo "# Entry added as part of Lingua Franca CLI tools installation:" >> ~/.bashrc
-  echo 'export PATH="${HOME}/.local/bin:${PATH}"' >> ~/.bashrc
-  echo "  ...please open a new terminal or run 'source ~/.bashrc'"
-fi
-
-echo ""
-echo "To verify that the installation was successful, run 'lfc --version'"
