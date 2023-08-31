f9ca0c1b48dd065882abbb428803294a184d409a
Marten Lohstroh <marten@berkeley.edu>
2023-08-30 21:53:36 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 set -eo pipefail
 
-# Installer for Lingua Franca tools.
-# 
-# To install the stable CLI tools, run:
-# curl -Ls https://install.lf-lang.org | bash -s cli
-# To install the nightly CLI tools, run:
-# curl -Ls https://install.lf-lang.org | bash -s nightly cli
-#
-# To install the stable release of Epoch, run:
-# curl -Ls https://install.lf-lang.org | bash -s epoch
-# To install the nightly release of Epoch, run:
-# curl -Ls https://install.lf-lang.org | bash -s nightly epoch
+# Docs: https://github.com/lf-lang/installation
+# Author: marten@berkeley.edu
+# License: BSD-2
+
 version="0.1.0-beta"
 tools=("cli" "epoch")
 selected=()
